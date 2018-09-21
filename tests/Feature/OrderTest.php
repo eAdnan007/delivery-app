ce0fa42a5bb81339e9d496e49165685f27fd7c56
Mohaimenul Adnan <adnan007.id@gmail.com>
2018-09-21 10:11:12 +0600

    Add tests for *place order* and *take order* endpoints

---
+++ tests/Feature/OrderTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Order;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class OrderTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $response = $this->json('POST', '/order', [
+		    'origin' => ['23.773214','90.409008'],
+            'destination' => ['23.752657', '90.375742']
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJsonStructure([
+                 	 'id', 'status', 'distance'
+                 ]);
+
+        $response->json('id');
+        Order::find('id')->delete();
+    }
+
+    public function testTake()
+    {
+    	$order = Order::create([
+    		'distance' => 1000,
+		    'status' => 'UNASSIGNED'
+	    ]);
+
+        $response_1 = $this->json('PUT', '/order/'.$order->id, [
+		    'status' => 'taken'
+        ]);
+
+        $response_1->assertStatus(200)
+                   ->assertExactJson([
+					    'status' => 'SUCCESS',
+					 ]);
+
+
+        // Same order should not be possible to take twice
+        $response_2 = $this->json('PUT', '/order/'.$order->id, [
+		    'status' => 'taken'
+        ]);
+
+        $response_2->assertStatus(409)
+                   ->assertExactJson([
+                   	     'error' => 'ORDER_ALREADY_BEEN_TAKEN',
+                   ]);
+
+        // Cleanup
+        $order->delete();
+    }
+}
